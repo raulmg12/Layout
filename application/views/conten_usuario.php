db2285b2453983e5e68462e6c1cc6289ed7ecc0d
raulmg12 <raulmg213@gmailcom>
2015-08-20 00:52:18 -0500

    Update conten_usuario.php

--- application/views/conten_usuario.php
+++ application/views/conten_usuario.php
@@ -4,20 +4,17 @@
 ?>
 
 <div class="row">
-  <div class="col-md-3 col-md-offset-2">
-    <h2>Usuario</h2>
-    <!--hay que cambiar la ruta de abajo el http-->
-    <a href="http://localhost/Layout/index.php/vistas/u">                  
-    <img src="<?php echo $rutaBase.'resources/imagenes/cinqueterre.jpg';?>" class="img-responsive" alt="Cinque Terre" width="204" height="136">
+  <div class="col-md-6">
+  <center><h2>Usuario</h2></center>
+  <!--hay que cambiar la ruta de abajo el http-->
+  <a href="<?php echo $rutaBase.'index.php/vistas/page/u';?>">  
+  <center><img src="<?php echo $rutaBase.'resources/imagenes/cinqueterre.jpg';?>" class="img-responsive" alt="Cinque Terre" width="204" height="136"></center>
+  </div>
+
+  <div class="col-md-6">
+  <center><h2>Graficas</h2></center>
+  <!--hay que cambiar la ruta de abajo el http-->
+  <a href="<?php echo $rutaBase.'index.php/vistas/page/g';?>">
+  <center><img src="<?php echo $rutaBase.'/resources/imagenes/grafica.jpg';?>" class="img-responsive" alt="Cinque Terre" width="304" height="236"></center> 
   </div>
 </div>
-
-<div class="row">
-  <div class="col-xs-3 col-md-offset-2">
-    <h2>Graficas</h2>   
-    <!--hay que cambiar la ruta de abajo el http-->
-    <a href="http://localhost/Layout/index.php/vistas/g"> 
-    <img src="<?php echo $rutaBase.'/resources/imagenes/grafica.jpg';?>" class="img-responsive" alt="Cinque Terre" width="304" height="236"> 
-  </div>
-</div>
-
