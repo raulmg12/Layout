ddeff53b26fc1fc25fc56271e4c78a83fe92754f
DaniGeraldine <gera08249091@gmail.com>
2015-08-19 22:17:11 -0500

    usuario

---
+++ application/views/u.php
@@ -0,0 +1,33 @@
+<!--Usuario-->
+
+<?php 
+/* obtiene la ruta del proyecto*/
+ $rutaBase = base_url();
+?>
+
+<div class="row">
+  <div class="col-md-6" style="padding:60px;">
+	  <center><img src="<?php echo $rutaBase.'resources/imagenes/user.png';?>" class="img-responsive" alt="Usuario"></center>
+  </div>
+  <div class="col-md-6">
+		<h1>Datos del usuario</h1>
+		</br>
+		</br>
+
+		<h4>Nombre:</h4> 
+		<p>Ricardo Rinc&oacute;n De la Torre</p>
+		</br>
+
+		<h4>Licenciatura:</h4>
+		<p>Computaci&oacute;n</p>
+		</br>
+
+		<h4>Edad:</h4>
+		<p>La sufuciente</p>
+		</br>
+
+		<h4>Pasatiempos:</h4>
+		<p>Escribir, leer y tocar guitarra cl&acute;sica</p>
+		</br>
+  </div>
+</div>
