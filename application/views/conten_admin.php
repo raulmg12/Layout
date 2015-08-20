3da39c787fbba09a47e8ca9d1d2880c1dd7fb1ca
DaniGeraldine <gera08249091@gmail.com>
2015-08-19 23:16:57 -0500

    Update conten_admin.php
    Modifique las rutas para enlazar las páginas individuales

--- application/views/conten_admin.php
+++ application/views/conten_admin.php
@@ -7,30 +7,30 @@
   <div class="col-md-3 col-md-offset-2">
     <h2>Usuario</h2>
 
-    <a href="http://localhost/Layout/index.php/vistas/page/u">                  
-    <img src="http://localhost/Layout/resources/imagenes/cinqueterre.jpg" class="img-responsive" alt="Cinque Terre" width="  204" height="136" > 
+    <a href="<?php echo $rutaBase.'index.php/vistas/u';?>">
+    <img src="<?php echo $rutaBase.'resources/imagenes/cinqueterre.jpg';?>" class="img-responsive" alt="Usuario" width="204" height="136" > 
   </div>
 
   <div class="col-md-3 col-md-offset-2">
     <h2>Configuracion</h2>
-    <a href="http://localhost/Layout/index.php/vistas/page/configuracion">                   
-    <img src="http://localhost/Layout/resources/imagenes/engrane.jpg" class="img-responsive" alt="Engrane" width="204" height="136" > </a>
 
+    <a href="<?php echo $rutaBase.'index.php/vistas/c';?>">                      
+    <img src="<?php echo $rutaBase.'resources/imagenes/engrane.jpg';?>" class="img-responsive" alt="Engrane" width="204" height="136" > 
   </div>
 </div>
 
 <div class="row">
   <div class="col-xs-3 col-md-offset-2">
-
-    <h2>Graficas</h2>                  
-    <a href="http://localhost/Layout/index.php/vistas/page/g"> 
-    <img src="http://localhost/Layout/resources/imagenes/grafica.jpg" class="img-responsive" alt="Cinque Terre" width="304" height="236"></a> 
+    <h2>Graficas</h2>     
+         
+    <a href="<?php echo $rutaBase.'index.php/vistas/g';?>">
+    <img src="<?php echo $rutaBase.'resources/imagenes/grafica.jpg';?>" class="img-responsive" alt="Grafica" width="304" height="236"> 
   </div>
 
   <div class="col-xs-3 col-md-offset-2">
     <h2>Tablas</h2>
-    <a href="http://localhost/Layout/index.php/vistas/page/t">                   
-    <img src="http://localhost/Layout/resources/imagenes/tabla.jpg" class="img-responsive" alt="Engrane" width="304" height="236"> 
 
+    <a href="<?php echo $rutaBase.'index.php/vistas/t';?>">                     
+    <img src="<?php echo $rutaBase.'resources/imagenes/tabla.jpg';?>" class="img-responsive" alt="Tabla" width="304" height="236"> 
   </div>
 </div>
