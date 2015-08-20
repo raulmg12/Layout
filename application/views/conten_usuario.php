bb73e1f27e7c7f112b00a3dafb7780f880a940cf
DaniGeraldine <gera08249091@gmail.com>
2015-08-19 20:24:41 -0500

    Update conten_usuario.php

--- application/views/conten_usuario.php
+++ application/views/conten_usuario.php
@@ -1,17 +1,23 @@
+<?php 
+/* obtiene la ruta del proyecto*/
+ $rutaBase = base_url();
+?>
+
 <div class="row">
   <div class="col-md-3 col-md-offset-2">
     <h2>Usuario</h2>
+    <!--hay que cambiar la ruta de abajo el http-->
     <a href="http://localhost/Layout/index.php/vistas/u">                  
-    <img src="http://localhost/Layout/resources/imagenes/cinqueterre.jpg" class="img-responsive" alt="Cinque Terre" width="204" height="136" > 
+    <img src="<?php echo $rutaBase.'resources/imagenes/cinqueterre.jpg';?>" class="img-responsive" alt="Cinque Terre" width="204" height="136">
   </div>
 </div>
 
 <div class="row">
   <div class="col-xs-3 col-md-offset-2">
-    <h2>Graficas</h2>                  
+    <h2>Graficas</h2>   
+    <!--hay que cambiar la ruta de abajo el http-->
     <a href="http://localhost/Layout/index.php/vistas/g"> 
-    <img src="http://localhost/Layout/resources/imagenes/grafica.jpg" class="img-responsive" alt="Cinque Terre" width="304" height="236"> 
+    <img src="<?php echo $rutaBase.'/resources/imagenes/grafica.jpg';?>" class="img-responsive" alt="Cinque Terre" width="304" height="236"> 
   </div>
 </div>
-
 
