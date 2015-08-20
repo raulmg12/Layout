86c359fc9796a491bb8358e19bb35052da5077df
DaniGeraldine <gera08249091@gmail.com>
2015-08-19 19:40:34 -0500

    Update conten_usuario.php

--- application/views/conten_usuario.php
+++ application/views/conten_usuario.php
@@ -1,21 +1,17 @@
 <div class="row">
   <div class="col-md-3 col-md-offset-2">
     <h2>Usuario</h2>
-    <a href="http://localhost/Codeigniter/index.php/vistas/u">                  
-    <img src="http://localhost/CodeIgniter/resources/imagenes/cinqueterre.jpg" class="img-responsive" alt="Cinque Terre" width="204" height="136" > 
+    <a href="http://localhost/Layout/index.php/vistas/u">                  
+    <img src="http://localhost/Layout/resources/imagenes/cinqueterre.jpg" class="img-responsive" alt="Cinque Terre" width="204" height="136" > 
   </div>
 </div>
 
 <div class="row">
   <div class="col-xs-3 col-md-offset-2">
     <h2>Graficas</h2>                  
-    <a href="http://localhost/Codeigniter/index.php/vistas/g"> 
-    <img src="http://localhost/CodeIgniter/resources/imagenes/grafica.jpg" class="img-responsive" alt="Cinque Terre" width="304" height="236"> 
+    <a href="http://localhost/Layout/index.php/vistas/g"> 
+    <img src="http://localhost/Layout/resources/imagenes/grafica.jpg" class="img-responsive" alt="Cinque Terre" width="304" height="236"> 
   </div>
 </div>
 
 
-</div>
-
-</body>
-</html>
