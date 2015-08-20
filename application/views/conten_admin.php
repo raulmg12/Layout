d05d15ee34954203bf3d4750eda3e1f778f6f1c2
DaniGeraldine <gera08249091@gmail.com>
2015-08-19 20:44:10 -0500

    Update conten_admin.php

--- application/views/conten_admin.php
+++ application/views/conten_admin.php
@@ -1,32 +1,36 @@
+<?php 
+/* obtiene la ruta del proyecto*/
+ $rutaBase = base_url();
+?>
 
 <div class="row">
   <div class="col-md-3 col-md-offset-2">
     <h2>Usuario</h2>
-    <a href="http://localhost/Layout/index.php/vistas/u">                  
-    <img src="http://localhost/Layout/resources/imagenes/cinqueterre.jpg" class="img-responsive" alt="Cinque Terre" width="  204" height="136" > 
+    <!--cambiar la ruta del href-->
+    <a href="http://localhost/Codeigniter/index.php/vistas/u">                  
+    <img src="<?php echo $rutaBase.'resources/imagenes/cinqueterre.jpg';?>" class="img-responsive" alt="Cinque Terre" width="  204" height="136" > 
   </div>
 
   <div class="col-md-3 col-md-offset-2">
     <h2>Configuracion</h2>
-    <a href="http://localhost/Layout/index.php/vistas/c">                   
-    <img src="http://localhost/Layout/resources/imagenes/engrane.jpg" class="img-responsive" alt="Engrane" width="204" height="136" > 
+        <!--cambiar la ruta del href-->
+    <a href="http://localhost/Codeigniter/index.php/vistas/c">                   
+    <img src="<?php echo $rutaBase.'resources/imagenes/engrane.jpg';?>" class="img-responsive" alt="Engrane" width="204" height="136" > 
   </div>
 </div>
 
 <div class="row">
   <div class="col-xs-3 col-md-offset-2">
-    <h2>Graficas</h2>                  
-    <a href="http://localhost/Layout/index.php/vistas/g"> 
-    <img src="http://localhost/Layout/resources/imagenes/grafica.jpg" class="img-responsive" alt="Cinque Terre" width="304" height="236"> 
+    <h2>Graficas</h2>          
+        <!--cambiar la ruta del href-->
+    <a href="http://localhost/Codeigniter/index.php/vistas/g"> 
+    <img src="<?php echo $rutaBase.'resources/imagenes/grafica.jpg';?>" class="img-responsive" alt="Grafica" width="304" height="236"> 
   </div>
 
   <div class="col-xs-3 col-md-offset-2">
     <h2>Tablas</h2>
-    <a href="http://localhost/Layout/index.php/vistas/t">                   
-    <img src="http://localhost/Layout/resources/imagenes/tabla.jpg" class="img-responsive" alt="Engrane" width="304" height="236"> 
+        <!--cambiar la ruta del href-->
+    <a href="http://localhost/Codeigniter/index.php/vistas/t">                   
+    <img src="<?php echo $rutaBase.'resources/imagenes/tabla.jpg';?>" class="img-responsive" alt="Tabla" width="304" height="236"> 
   </div>
 </div>
-
-
-</body>
-</html>
